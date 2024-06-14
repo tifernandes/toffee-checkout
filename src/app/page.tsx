959c554dee09f41f60a2d4d8f709fc98276f60ab
Tiago Fernandes <tiago.fernandes@timeprimo.com>
2024-06-14 19:23:15 -0300

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,174 @@
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+import { Tabs } from "@/components/ui/tabs";
+import { User, Truck, CreditCard } from 'lucide-react'
+import { Boxes } from "@/components/ui/background-boxes";
+
 export default function Home() {
+
+    const tabs = [
+      {
+        title: (
+          <div className="flex flex-row items-center justify-center gap-2">
+            <User color="black" size={28} /> 
+            <h1>Dados</h1>
+          </div>
+        ),
+        value: "dados",
+        content: (
+          <div>
+          {/* STEP 1 */}
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>Dados pessoais</CardTitle>
+                <CardDescription>Solicitamos apenas as informações essenciais para concluir a compra.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form>
+                  <div className="grid w-full items-center gap-4">
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="name">Nome</Label>
+                      <Input id="name" placeholder="Seu nome completo" />
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="name">E-Mail</Label>
+                      <Input id="email" type="email" placeholder="Seu melhor e-mail" />
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="name">CPF</Label>
+                      <Input id="email" placeholder="000.000.000-00" />
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="name">Celular</Label>
+                      <Input id="email" placeholder="Celular para contato se necessário" />
+                    </div>
+                  </div>
+                </form>
+              </CardContent>
+              <CardFooter className="flex justify-between">
+                
+                <Button>Continuar</Button>
+              </CardFooter>
+            </Card>
+          </div>
+        ),
+      },
+      {
+        title: (
+          <div className="flex flex-row items-center justify-center gap-2">
+            <Truck color="black" size={28} /> 
+            <h1>Entrega</h1>
+          </div>
+        ),
+        value: "entrega",
+        content: (
+          <div>
+          {/* STEP 2 */}
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>Entrega</CardTitle>
+                <CardDescription>Cadastre um endereço que receberá a entrega.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form>
+                  <div className="grid w-full items-center gap-4">
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="name">CEP</Label>
+                      <Input id="name" placeholder="00000-000" />
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="name">Número</Label>
+                      <Input id="name" placeholder="11" />
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="name">Complemento (opcional)</Label>
+                      <Input id="name" placeholder="" />
+                    </div>
+                  </div>
+                </form>
+              </CardContent>
+              <CardFooter className="flex justify-between">
+                
+                <Button>Continuar</Button>
+              </CardFooter>
+            </Card>
+          </div>
+        ),
+      },
+      {
+        title: (
+          <div className="flex flex-row items-center justify-center gap-2">
+            <CreditCard color="black" size={28} /> 
+            <h1>Pagamento</h1>
+          </div>
+        ),
+        value: "pagamento",
+        content: (
+          <div>
+          {/* STEP 3 */}
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>Pagamento</CardTitle>
+                <CardDescription>Escolha sua forma de pagamento preferida.</CardDescription>
+              </CardHeader>
+              <CardContent>
+
+              </CardContent>
+              <CardFooter className="flex justify-between">
+                
+                <Button>Finalizar</Button>
+              </CardFooter>
+            </Card>
+          </div>
+        ),
+      }
+    ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="w-full flex justify-center flex-col items-center gap-7">
+      <div className="h-36 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
+        <div className="absolute inset-0 w-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+        <Boxes />
+        <p className="text-center mt-2 text-neutral-300 relative z-20">
+          Framer motion is the best animation library ngl
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+      <div className="flex flex-col items-center max-w-7xl gap-7 lg:flex-row lg:flex-row-reverse xl:w-full lg:items-start">
+        <div className="sm:w-[90%] bg-white max-w-sm">
+          <Card className="w-full">
+                <CardHeader>
+                  <CardTitle>Resumo</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                  
+                </CardFooter>
+          </Card>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="sm:w-[90%] bg-white flex flex-col max-w-3xl justify-center">
+          <Tabs tabs={tabs} />
+        </div>
       </div>
     </main>
   );
